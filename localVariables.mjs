4e0aa712061d8f4cc7137a5012867bd4045be185
Joxbard <83847909+Joxbard@users.noreply.github.com>
2021-05-07 19:47:36 -0500

    Create localVariables.mjs

---
+++ localVariables.mjs
@@ -0,0 +1,3 @@
+export const applicationId = '840334165084995636';
+export const guildId = '811978693932482560';
+export const botToken = 'ODQwMzM0MTY1MDg0OTk1NjM2.YJWsLQ.cSo-FAaAB2Jn5sodKlg_zBylJsU';
